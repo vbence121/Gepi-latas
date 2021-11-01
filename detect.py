cbcf49b6b01d6ceb839949a131e197f0c8bd32a4
vbence121 <vbence121@gmail.com>
2021-11-01 15:00:01 +0100

    Added batch and "silent" mode to detect.py

--- detect.py
+++ detect.py
@@ -6,45 +6,51 @@ import numpy as np
 import pytesseract
 import getopt
 import sys
+from os import listdir
 from pathlib import Path
-
+import re
 from pytesseract.pytesseract import main
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 verbose = False     # if enabled images are displayed while detection is in progress. 
 verboseF = False    # if enabled images are displayed while detection is in progress (they are all displayed at the same time). 
-helpMSG = "detect.py -i <inputfile> [-v/-V]"
+helpMSG = "detect.py -i <inputfile> [-v/-V] [-b] [-S]"
+batchM = False      # enable batch mode
+silentM = False     # enable silent mode
 
 inputfile = "" # file to be read
 Ffilter = "-" # filter used on result when comparing to expected output
 Gsize = (600,400)
 
-def match() -> bool:
+def match(file:str) -> bool:
     """
     Try to read image file and check result against expected license plate.
     """
-    if(inputfile == ""):
+    if(file == ""):
         print(helpMSG)
-    if(not(Path(inputfile).exists())):
+    if(not(Path(file).exists())):
         print("File at given path does not exist.")
         sys.exit(-1)
-    res = detect()
+    res = detect(file)
 
-    expct = Path(inputfile).stem
+    expct = Path(file).stem
     for s in Ffilter:
         res = res.replace(s,"")
-    print("exp:"+str.upper(expct).strip()+" result:"+str.upper(res).strip())
+    if(batchM):
+        print("exp:"+str.upper(expct).strip()+" result:"+str.upper(res).strip(),end="")
+    else:
+        print("exp:"+str.upper(expct).strip()+" result:"+str.upper(res).strip())
     if(str.upper(expct).strip() == str.upper(res).strip()):
         return True
     else:
         return False
 
 
-def detect() -> str:
-    if(not(Path(inputfile).exists()) or inputfile == ""):
+def detect(file:str) -> str:
+    if(not(Path(file).exists()) or file == ""):
         print("File at given path does not exist.")
         sys.exit(-1)
-    img = cv.imread(inputfile)
+    img = cv.imread(file)
     img = cv.resize(img, (600,400))
     if(verbose or verboseF):
         cv.imshow("Color", img)
@@ -95,20 +101,21 @@ def detect() -> str:
     Cropped = gray[topx:bottomx+1, topy:bottomy+1]
 
     text = pytesseract.image_to_string(Cropped, config='--psm 11')
-    print("Detected license plate Number is:",text[:-1].strip())
+    text = re.sub(r"[^a-zA-Z0-9 ]", "", text)
+    print("Detected license plate Number is:",text)
     img = cv.resize(img,(500,300))
     Cropped = cv.resize(Cropped,(400,200))
-    cv.imshow('Original',img)
-    cv.imshow('Cropped',Cropped)
-
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-    return text[:-1]
+    if(not(silentM)):
+        cv.imshow('Original',img)
+        cv.imshow('Cropped',Cropped)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+    return text
 
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hi:s:Vv")
+        opts, args = getopt.getopt(sys.argv[1:], "hi:s:VvbS")
     except getopt.getopt.GetoptError:
         print(helpMSG)
         sys.exit(2)
@@ -119,7 +126,7 @@ def main():
         elif opt in ("-i"):
             global inputfile 
             inputfile = arg
-            if(not(Path(arg).exists())):
+            if(not(Path(arg).exists()) or not(Path(inputfile).is_dir())):
                 print("File at given path does not exist.")
                 sys.exit(-1)
         elif opt in ("-s"):
@@ -131,11 +138,28 @@ def main():
         elif opt == "-V":
             global verboseF 
             verboseF = True
+        elif opt == "-b":
+            global batchM 
+            batchM = True
+        elif opt == "-S":
+            global silentM 
+            silentM = True
 
-    if(match()):
-        print("Matching!")
+    if(batchM and Path(inputfile).is_dir()):
+        correct = 0
+        all = len(listdir(inputfile))
+        for file in listdir(inputfile):
+            if(match(inputfile+file)):
+                correct = correct+1
+                print(" Matching!")
+            else:
+                print(" Result does not match or image detection failed.")
+        print("\nMatch ratio: "+str(correct)+"\\"+str(all))
     else:
-        print("Result does not match or image detection failed.")
+        if(match(inputfile)):
+            print("Matching!")
+        else:
+            print("Result does not match or image detection failed.")
 
 
 if __name__ == "__main__":
